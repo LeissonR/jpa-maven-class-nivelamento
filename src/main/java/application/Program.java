f84a450016ff73b0156bb59268ad3779e630f43f
Leisson Rodrigues <leisson@gmail.com>
2023-05-06 02:06:32 -0400

    Program created and mapping the People Class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,18 +1,32 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import entities.People;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		People p1 = new People(1, "Nac Red", "nacRR@nac.com");
-		People p2 = new People(2, "Nac Brown", "nacBB@nac.com");
-		People p3 = new People(3, "Nac Green", "nacGG@nac.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		People p1 = new People(null, "1Nac Red", "nacRR@nac.com");
+		People p2 = new People(null, "2Nac Brown", "nacBB@nac.com");
+		People p3 = new People(null, "3Nac Green", "nacGG@nac.com");
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		
+		System.out.println("Done!");
+		
+		em.close();
+		emf.close();
 	}
 
 }
