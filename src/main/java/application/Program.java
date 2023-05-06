bcb47ebad0e51cb0f7df57f0288715298e55ca3e
Leisson Rodrigues <leisson@gmail.com>
2023-05-06 02:13:26 -0400

    find the object by Id

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -9,22 +9,15 @@ import entities.People;
 public class Program {
 
 	public static void main(String[] args) {
-	
-		People p1 = new People(null, "1Nac Red", "nacRR@nac.com");
-		People p2 = new People(null, "2Nac Brown", "nacBB@nac.com");
-		People p3 = new People(null, "3Nac Green", "nacGG@nac.com");
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		
+
+		People p = em.find(People.class, 2);
+
+		System.out.println(p);
 		System.out.println("Done!");
-		
+
 		em.close();
 		emf.close();
 	}
